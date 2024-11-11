89f14b4864d5377f1518e6f38ddda31e7b5effae
clayzim <clayzim@umich.edu>
2024-11-10 23:06:27 -0500

    Created collection list csv from json data

--- collection_list.py
+++ collection_list.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import csv
 
 def get_collection_list(url):
     response = requests.get(url, params={"fo": "json"})
@@ -16,3 +17,21 @@ def download_libraries_json():
         output = json.dumps(items["content"], sort_keys = True, indent = 4)
         file.write(output)
 
+def create_csv_list_from_json(infile, outfile):
+    with open(infile, mode='r', encoding="utf-8") as response:
+        content = json.load(response)
+
+    items = content["set"]["items"]
+
+    with open(outfile, mode='w', encoding="utf-8") as csv_file:
+        output = csv.writer(csv_file)
+
+        output.writerow(["image", "link", "title"])
+        for item in items:
+            image = item["image"] if "image" in item else "NO_IMAGE"
+            link = item["link"] if "link" in item else "NO_LINK"
+            title = item["title"] if "title" in item else "NO_TITLE"
+            output.writerow([image, link, title])
+
+def create_csv_list():
+    create_csv_list_from_json("./response.json", "./ftu-libraries-set-list.csv")
