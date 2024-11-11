e6408847f546e53b708d248e6012a3d68301d43d
clayzim <clayzim@umich.edu>
2024-11-11 00:19:48 -0500

    Retrieved metadata for all but 3 items

--- collection-extract-tools.py
+++ collection-extract-tools.py
@@ -2,8 +2,12 @@ import requests
 import json
 import csv
 
+csv_filename = "./ftu-libraries-set-list.csv"
+fieldnames = ["image", "link", "title"]
+parameters = {"fo": "json"}
+
 def get_collection_list(url):
-    response = requests.get(url, params={"fo": "json"})
+    response = requests.get(url, params=parameters)
     if response.status_code == 200:
         return response.text
     else:
@@ -25,7 +29,7 @@ def create_csv_list_from_json(infile, outfile):
     with open(outfile, mode='w', encoding="utf-8") as csv_file:
         output = csv.writer(csv_file)
 
-        output.writerow(["image", "link", "title"])
+        output.writerow(fieldnames)
         for item in items:
             image = item["image"] if "image" in item else "NO_IMAGE"
             link = item["link"] if "link" in item else "NO_LINK"
@@ -35,8 +39,34 @@ def create_csv_list_from_json(infile, outfile):
 def extract_libraries_list():
     libraries_url = "https://www.loc.gov/free-to-use/libraries/"
     json_filename = "./ftu-libraries-set-info.json"
-    csv_filename = "./ftu-libraries-set-list.csv"
     download_collection_json(libraries_url, json_filename)
     create_csv_list_from_json(json_filename, csv_filename)
 
 extract_libraries_list()
+
+def retrieve_item_metadata(infile):
+    base_url = "https://www.loc.gov"
+    base_image_path = "./item-files/"
+    base_json_path = "./item-metadata/"
+    error_filename = "errors.txt"
+    with open(infile, mode='r', encoding="utf-8") as csv_file:
+        items = csv.DictReader(csv_file)
+        with open(error_filename, mode='w', encoding="utf-8") as errors:
+            for index, item in enumerate(items):
+                url = base_url + item["link"]
+                identifier = item["link"].split('/')[2]
+                response = requests.get(url, params=parameters)
+                if response.status_code != 200:
+                    errors.write(f"Index {index}: No HTTP OK\n")
+                content_type = response.headers["Content-Type"].split('/')[-1]
+                if content_type == "json":
+                    data = response.json()
+                    outfile = base_json_path + identifier + ".json"
+                    with open(outfile, mode='w', encoding="utf-8") as item_file:
+                        output = json.dumps(data, sort_keys = True, indent = 4)
+                        item_file.write(output)
+                else:
+                    errors.write(f"Index {index}: No JSON\n")
+
+
+retrieve_item_metadata(csv_filename)
